408eb79da003c4881909abab4130232531dbeb54
karabas94 <alkar_94@ukr.net>
2022-09-23 22:57:13 +0300

    range changed

--- homework5_exercise3.py
+++ homework5_exercise3.py
@@ -2,14 +2,14 @@ height = int(input("Enter altitude of triangle:"))
 
 print("Triangle 3")
 for h in range(height):               # перебор строк
-    for i in range(h, height-1):
+    for i in range(h, height):
         print(" ", end=" ")           # печать пробелов в строке
-    for j in range(h+1):
+    for j in range(h):
         print("*", end=" ")           # печать звездочек в строке
     print()                           # окончание строки
 for h in range(height):               # перебор строк
-    for j in range(h+1):
+    for j in range(h):
         print(" ", end=" ")           # печать пробелов в строке
-    for i in range(h, height - 1):
+    for i in range(h, height):
         print("*", end=" ")           # печать звездочек в строке
     print()                           # окончание строки
--- homework5_exercise4.py
+++ homework5_exercise4.py
@@ -2,10 +2,10 @@ height = int(input("Enter altitude of triangle:"))
 
 print("Triangle 4")
 for h in range(height):               # перебор строк
-    for i in range(h, height-1):
+    for i in range(h, height):
         print(" ", end=" ")           # печать пробелов в строке
-    for w in range(h):
+    for w in range(h+1):
         print("*", end=" ")           # печать звездочек в строке
-    for j in range(h+1):
+    for j in range(h):
         print("*", end=" ")           # печать звездочек в строке
     print()                           # окончание строки
